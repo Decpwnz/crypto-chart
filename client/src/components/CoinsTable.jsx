40398c3912813528aafdac5cf0b9640a75c6bb3d
Marijus <electromenn@gmail.com>
2023-09-06 17:19:37 +0300

    fix percentage change color, remove unused package

--- client/src/components/CoinsTable.jsx
+++ client/src/components/CoinsTable.jsx
@@ -78,70 +78,65 @@ function CoinsTable() {
             </TableHead>
             <TableBody>
               {coins
-                .map((row) => {
-                  const profit = row.price_perpercentage_24h > 0;
-
-                  return (
-                    <TableRow
-                      onClick={() => handleCryptoSelected(row.id)}
-                      key={row.name}
+                .map((row) => (
+                  <TableRow
+                    onClick={() => handleCryptoSelected(row.id)}
+                    key={row.name}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      style={{
+                        display: 'flex',
+                        gap: 15,
+                      }}
                     >
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{
-                          display: 'flex',
-                          gap: 15,
-                        }}
+                      <img
+                        src={row?.image}
+                        alt={row.name}
+                        height="50"
+                        style={{ marginBottom: 10 }}
+                      />
+                      <div
+                        style={{ display: 'flex', flexDirection: 'column' }}
                       >
-                        <img
-                          src={row?.image}
-                          alt={row.name}
-                          height="50"
-                          style={{ marginBottom: 10 }}
-                        />
-                        <div
-                          style={{ display: 'flex', flexDirection: 'column' }}
+                        <span
+                          style={{
+                            textTransform: 'uppercase',
+                            fontSize: 22,
+                          }}
                         >
-                          <span
-                            style={{
-                              textTransform: 'uppercase',
-                              fontSize: 22,
-                            }}
-                          >
-                            {row.symbol}
-                          </span>
-                          <span style={{ color: 'darkgrey' }}>
-                            {row.name}
-                          </span>
-                        </div>
-                      </TableCell>
-                      <TableCell>
-                        {symbol}
-                        {' '}
-                        {numberWithCommas(row.current_price.toFixed(2))}
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          color: profit > 0 ? 'rgb(14, 203, 129)' : 'red',
-                          fontWeight: 500,
-                        }}
-                      >
-                        {profit && '+'}
-                        {row.price_change_percentage_24h.toFixed(2)}
-                        %
-                      </TableCell>
-                      <TableCell>
-                        {symbol}
-                        {' '}
-                        {numberWithCommas(
-                          row.market_cap.toString().slice(0, -6),
-                        )}
-                        M
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
+                          {row.symbol}
+                        </span>
+                        <span style={{ color: 'darkgrey' }}>
+                          {row.name}
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {symbol}
+                      {' '}
+                      {numberWithCommas(row.current_price.toFixed(2))}
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        color: row.market_cap_change_percentage_24h > 0 ? 'green' : 'red',
+                        fontWeight: 500,
+                      }}
+                    >
+                      {row.market_cap_change_percentage_24h.toFixed(2)}
+                      %
+                    </TableCell>
+                    <TableCell>
+                      {symbol}
+                      {' '}
+                      {numberWithCommas(
+                        row.market_cap.toString().slice(0, -6),
+                      )}
+                      M
+                    </TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
         )}
